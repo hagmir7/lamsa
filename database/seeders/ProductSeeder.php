74177fd33e5f6afa463e6e01beb4e750c4d11cf6
Hassan Agmir <hagmir6@gmail.com>
2024-04-24 10:34:52 +0100

    Adding bags

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,7 +15,7 @@ class ProductSeeder extends Seeder
         $products = [
             [
                 'name' => 'Casual Dress',
-                'category_id' => 3, // Assuming 'Dresses' category ID is 3
+                'category_id' => 1, // Assuming 'Dresses' category ID is 3
                 'price' => 49.99,
                 'description' => 'A casual dress suitable for everyday wear.',
                 'status' => 1, // 1 for available, 0 for out of stock, etc.
@@ -24,7 +24,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'name' => 'Leather Jacket',
-                'category_id' => 4, // Assuming 'Jackets' category ID is 4
+                'category_id' => 1, // Assuming 'Jackets' category ID is 4
                 'price' => 89.99,
                 'description' => 'A stylish leather jacket to keep you warm in style.',
                 'status' => 1,
@@ -33,7 +33,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'name' => 'Denim Skirt',
-                'category_id' => 5, // Assuming 'Skirts' category ID is 5
+                'category_id' => 1, // Assuming 'Skirts' category ID is 5
                 'price' => 29.99,
                 'description' => 'A trendy denim skirt for a fashionable look.',
                 'status' => 1,
@@ -42,7 +42,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'name' => 'Striped Sweater',
-                'category_id' => 6, // Assuming 'Sweaters' category ID is 6
+                'category_id' => 2, // Assuming 'Sweaters' category ID is 6
                 'price' => 34.99,
                 'description' => 'A cozy sweater with stylish stripes.',
                 'status' => 1,
@@ -51,7 +51,7 @@ class ProductSeeder extends Seeder
             ],
             [
                 'name' => 'Cargo Shorts',
-                'category_id' => 7, // Assuming 'Shorts' category ID is 7
+                'category_id' => 2, // Assuming 'Shorts' category ID is 7
                 'price' => 24.99,
                 'description' => 'Comfortable cargo shorts for outdoor activities.',
                 'status' => 1,
@@ -60,7 +60,17 @@ class ProductSeeder extends Seeder
             ],
             [
                 'name' => 'Silk Blouse',
-                'category_id' => 8, // Assuming 'Blouses' category ID is 8
+                'category_id' => 2, // Assuming 'Blouses' category ID is 8
+                'price' => 54.99,
+                'description' => 'An elegant silk blouse for a sophisticated look.',
+                'status' => 1,
+                'body' => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...',
+                'slug' => Str::slug("Silk Blouse")
+            ],
+
+            [
+                'name' => 'Sac Tote En Cuir',
+                'category_id' => 2, // Assuming 'Blouses' category ID is 8
                 'price' => 54.99,
                 'description' => 'An elegant silk blouse for a sophisticated look.',
                 'status' => 1,
@@ -178,6 +188,24 @@ class ProductSeeder extends Seeder
             [
                 'path' => 'products/img-24.webp',
                 'product_id' => 6,
+            ],
+
+
+            [
+                'path' => 'products/img-25.jpg',
+                'product_id' => 7, // Assuming product ID is 3
+            ],
+            [
+                'path' => 'products/img-26.jpg',
+                'product_id' => 7,
+            ],
+            [
+                'path' => 'products/img-27.jpg',
+                'product_id' => 7, // Assuming product ID is 3
+            ],
+            [
+                'path' => 'products/img-28.jpg',
+                'product_id' => 7,
             ],
         ];
 
