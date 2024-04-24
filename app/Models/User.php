719dba0b67482293e0c9c28b4100b89e69827bfe
Hassan Agmir <hagmir6@gmail.com>
2024-04-24 13:46:13 +0100

    Update access

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,12 +3,16 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Filament\Panel;
 
-class User extends Authenticatable
+
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens, HasFactory, Notifiable;
 
@@ -23,6 +27,14 @@ class User extends Authenticatable
         'password',
         'cart_id'
     ];
+
+
+    public function canAccessPanel(Panel $panel): bool
+    {
+        return str_ends_with($this->email, '@admin.com');
+    }
+
+
 
     /**
      * The attributes that should be hidden for serialization.
