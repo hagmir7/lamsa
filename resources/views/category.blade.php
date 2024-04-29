6804a6604bcf6c3251d192581f07440d77920eec
Hassan Agmir <hagmir6@gmail.com>
2024-04-29 21:18:07 +0100

    Update cateogyr font

--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -3,9 +3,9 @@
 <div class="banner-wrapper my-wrapper has_background">
     <img src="{{ Storage::url($category->image) }}" style="width: 100%;max-height: 300px;" class="img-responsive attachment-1920x447 size-1920x447 hero-bg-img" alt="img">
     <div class="banner-wrapper-inner">
-        <h1 class="page-title">{{ $category->name }}</h1>
+        <h1 class="page-title text-black">{{ $category->name }}</h1>
         <div role="navigation" aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs">
-            <div class="fw-bold h5" style="line-height: 29px">{{ $category->description }}</div>
+            <div class="fw-bold h6" style="line-height: 29px;">{{ $category->description }}</div>
         </div>
     </div>
 </div>
