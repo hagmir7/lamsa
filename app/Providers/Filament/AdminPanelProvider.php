5c9a94c59c2ad348e0e72c2c80cb0a9985096e7c
Hassan Agmir <hagmir6@gmail.com>
2024-07-10 12:33:34 +0100

    Update panel logo

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -29,7 +29,7 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
             ->login()
             ->brandLogoHeight('75px')
-            ->brandLogo('/assets/imgs/lamsa.png')
+            ->brandLogo('/assets/imgs/rounded-logo.png')
             ->colors([
                 'primary' => Color::Amber,
             ])
